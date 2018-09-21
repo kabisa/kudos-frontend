597e8af597f8147629306a2e29a58e931cae054e
Stefan Pahlplatz <stefan314@live.nl>
2018-09-21 16:12:00 +0200

    🔧  Furthered the plans to make @egonm12 superfluous

--- kudos-o-matic/mock/server.js
+++ kudos-o-matic/mock/server.js
@@ -1,4 +1,5 @@
 /* eslint import/no-extraneous-dependencies: off */
+/* eslint no-console: 0 */
 /**
  * Mock server
  *
@@ -9,21 +10,80 @@
  * - password: password
  */
 const jsonServer = require('json-server');
+const faker = require('faker');
 
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+// Functions
+
+function generateTransactions() {
+  const transactions = [];
+  for (let id = 0; id < 50; id += 1) {
+    transactions.push({
+      id,
+      author_id: Math.floor(Math.random() * 10),
+      message: faker.hacker.phrase(),
+      kudos: Math.floor(Math.random() * 50),
+      comments: Math.floor(Math.random() * 50),
+      likes: Math.floor(Math.random() * 50)
+    });
+  }
+  return transactions;
+}
+
+function generateUsers() {
+  const users = [];
+  for (let id = 0; id < 10; id += 1) {
+    users.push({
+      id,
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email(),
+      avatar_url: faker.internet.avatar(),
+      description: faker.hacker.phrase()
+    });
+  }
+  return users;
+}
 
 function isAuthorized(req) {
   return req.headers.token === '123';
 }
 
-server.use((req, res, next) => {
-  setTimeout(next, 250);
-});
+const schema = {
+  transactions: generateTransactions(),
+  users: generateUsers()
+};
+
+const server = jsonServer.create();
+const router = jsonServer.router(schema);
+const middlewares = jsonServer.defaults();
+
+// Middleware
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  // Request delay
+  setTimeout(next, 250);
+});
+server.use((req, res, next) => {
+  // Authentication
+  if (req.path === '/login') {
+    next();
+  } else if (isAuthorized(req)) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+});
+server.use((req, res, next) => {
+  // Log all post requests
+  if (req.method === 'POST') {
+    console.log(req.body);
+  }
+  next();
+});
+
+// Routes
 
 server.post('/login', (req, res) => {
   if (req.body.username === 'username' && req.body.password === 'password') {
@@ -33,16 +93,7 @@ server.post('/login', (req, res) => {
   }
 });
 
-server.use((req, res, next) => {
-  if (isAuthorized(req)) {
-    next();
-  } else {
-    res.sendStatus(401);
-  }
-});
-
 server.use(router);
 server.listen(3001, () => {
-  /* eslint no-console: 0 */
   console.log('JSON Server is running');
 });
