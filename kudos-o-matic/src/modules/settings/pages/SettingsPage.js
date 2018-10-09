1199fbd17346408661aef169cf07266517f88693
Stefan Pahlplatz <stefan314@live.nl>
2018-10-09 15:40:21 +0200

    ♻️  Improved settings page

--- kudos-o-matic/src/modules/settings/pages/SettingsPage.js
+++ kudos-o-matic/src/modules/settings/pages/SettingsPage.js
@@ -8,12 +8,12 @@ import { Navigation } from '../../navigation';
 
 export const SettingsPage = () => (
   <div style={{ height: '100%' }}>
-    <div className="page flex">
+    <div className="page flex" style={{ justifyContent: 'space-around' }}>
       <div>
         <h2>Settings</h2>
       </div>
-      <Link color="orange" className="button" to={PATH_CHOOSE_TEAM}>
-        <Button>Switch team</Button>
+      <Link to={PATH_CHOOSE_TEAM}>
+        <Button color="orange">Switch team</Button>
       </Link>
     </div>
     <Navigation />
