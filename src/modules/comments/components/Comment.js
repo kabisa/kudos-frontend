92682c7666f1831cd1da6ef1417b29654a6ce54c
Stefan Pahlplatz <stefan314@live.nl>
2018-11-13 11:11:50 +0100

    Squash

---
+++ src/modules/comments/components/Comment.js
@@ -0,0 +1,19 @@
+import { h } from "preact";
+
+const Comment = () => (
+  <Comment>
+    <Comment.Avatar src="https://s3.amazonaws.com/uifaces/faces/twitter/exentrich/128.jpg" />
+    <Comment.Content>
+      <Comment.Author as="a">Matt</Comment.Author>
+      <Comment.Metadata>
+        <div>Today at 5:42PM</div>
+      </Comment.Metadata>
+      <Comment.Text>How artistic!</Comment.Text>
+      <Comment.Actions>
+        <Comment.Action>Reply</Comment.Action>
+      </Comment.Actions>
+    </Comment.Content>
+  </Comment>
+);
+
+export default Comment;
