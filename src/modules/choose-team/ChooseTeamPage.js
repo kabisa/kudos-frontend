f0148dc9873c453542542803f719a74cfcaf6918
Stefan Pahlplatz <stefan314@live.nl>
2018-11-21 15:40:29 +0100

    Team invites

--- src/modules/choose-team/ChooseTeamPage.js
+++ src/modules/choose-team/ChooseTeamPage.js
@@ -1,29 +1,11 @@
 import { h, Component } from "preact";
-import { Query } from "react-apollo";
+import { Divider } from "semantic-ui-react";
 
 import { Toolbar } from "../../components/navigation";
-import { TeamRow } from "./components";
+import { TeamList, InviteList } from "./components";
 import { isLoggedIn } from "../../support";
-import { GET_TEAMS } from "./queries";
 
 import s from "./ChooseTeamPage.scss";
-
-const TeamList = () => (
-  <Query query={GET_TEAMS} pollInterval={2000}>
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return `Error! ${error.message}`;
-
-      return (
-        <div>
-          {data.teams.map(team => (
-            <TeamRow id={team.id} name={team.name} key={team.id} />
-          ))}
-        </div>
-      );
-    }}
-  </Query>
-);
 
 export class ChooseTeamPage extends Component {
   constructor(props) {
@@ -36,6 +18,9 @@ export class ChooseTeamPage extends Component {
       <div>
         <Toolbar text="Choose a group" />
         <div className={s.page}>
+          <h2 className={s.header}>Your invites</h2>
+          <InviteList />
+          <Divider />
           <h2 className={s.header}>Your teams</h2>
           <TeamList />
         </div>
@@ -45,9 +30,3 @@ export class ChooseTeamPage extends Component {
 }
 
 export default ChooseTeamPage;
-
-// <h2>Your invites</h2>
-// <Invite />
-// <Invite />
-// <Invite />
-// <Divider />
--- src/modules/choose-team/components/Invite.js
+++ src/modules/choose-team/components/Invite.js
@@ -1,17 +1,40 @@
 import { h } from "preact";
-import { Button } from "semantic-ui-react";
+import gql from "graphql-tag";
+
+import ChoiceButton from "./ChoiceButton";
 
 import s from "./style.scss";
 
-export const Invite = () => (
+export const MUTATION_ACCEPT_INVITE = gql`
+  mutation AcceptInvite($team_invite_id: ID!) {
+    acceptInvite(team_invite_id: $team_invite_id) {
+      id
+    }
+  }
+`;
+export const MUTATION_DECLINE_INVITE = gql`
+  mutation DeclineInvite($team_invite_id: ID!) {
+    declineInvite(team_invite_id: $team_invite_id) {
+      id
+    }
+  }
+`;
+
+export const Invite = ({ invite }) => (
   <div className={s.root}>
-    <p className={s.text}>Kabisa</p>
-    <Button color="green" size="small" className={s.button}>
-      Accept
-    </Button>
-    <Button color="orange" size="small" className={s.button}>
-      Decline
-    </Button>
+    <p className={s.text}>{invite.team.name}</p>
+    <ChoiceButton
+      inviteId={invite.id}
+      mutation={MUTATION_DECLINE_INVITE}
+      text="Decline"
+      color="orange"
+    />
+    <ChoiceButton
+      inviteId={invite.id}
+      mutation={MUTATION_ACCEPT_INVITE}
+      text="Accept"
+      color="red"
+    />
   </div>
 );
 
