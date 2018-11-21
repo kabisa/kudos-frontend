637f3bc56a8c7dcef9d4784fa4ce8efda006fedc
Stefan Pahlplatz <stefan314@live.nl>
2018-11-21 14:47:02 +0100

    Finished forgot password flow

--- src/modules/login/index.js
+++ src/modules/login/index.js
@@ -1,3 +1,6 @@
 export { default as LoginPage } from "./LoginPage";
 export { default as RegisterPage } from "./RegisterPage";
 export { default as ForgotPasswordPage } from "./ForgotPasswordPage";
+export {
+  default as FinishForgotPasswordPage,
+} from "./FinishForgotPasswordPage";
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 export const PATH_REGISTER = "/register";
 export const PATH_FORGOT_PASSWORD = "/forgot-password";
 export const PATH_RESET_PASSWORD = "/reset-password";
+export const PATH_FINISH_RESET_PASSWORD = "/users/password/edit";
 export const PATH_LOGIN = "/login";
 export const PATH_SETTINGS = "/settings";
 export const PATH_STATISTICS = "/statistics";
