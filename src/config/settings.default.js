4e48266a7277b878dfb7bad0121b56d58be89224
Stefan Pahlplatz <stefan314@live.nl>
2018-11-27 11:31:20 +0100

    Improved error messages

--- src/config/settings.default.js
+++ src/config/settings.default.js
@@ -1,6 +1,14 @@
 export default {
+  // App settings.
   defaultLocale: "en-US",
+
+  // Backend settings.
+  MIN_PASSWORD_LENGTH: 8,
+
+  // Network settings.
   API_BASE_URL: "https://kudo-o-matic-staging.dokku.kabisa.io",
+
+  // localStorage settings.
   LOCALSTORAGE_TOKEN: "token",
   TEAM_ID_TOKEN: "team_id",
   USER_ID_TOKEN: "user_id",
