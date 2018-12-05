c859d2b60b099a0964bd237e35e8a6831884a732
Stefan Pahlplatz <stefan314@live.nl>
2018-12-05 09:06:21 +0100

    Better error checking

--- src/modules/user/ResetPasswordPage.js
+++ src/modules/user/ResetPasswordPage.js
@@ -7,11 +7,11 @@ import { toast } from "react-toastify";
 import settings from "../../config/settings";
 import {
   auth,
-  getGraphqlError,
-  ERROR_SHORT_PASSWORD,
   ERROR_PASSWORD_BLANK,
-  ERROR_PASSWORD_MATCH,
   ERROR_PASSWORD_CONFIRMATION_BLANK,
+  ERROR_PASSWORD_MATCH,
+  ERROR_SHORT_PASSWORD,
+  getGraphqlError,
 } from "../../support";
 import { Navigation, Toolbar } from "../../components/navigation";
 
@@ -37,18 +37,70 @@ class ResetPasswordPage extends Component {
   constructor(props) {
     super(props);
 
+    this.cleanErrors = {
+      error: null,
+      error_current: null,
+      error_new: null,
+      error_new_confirm: null,
+    };
+
     this.state = {
       current_password: "",
       new_password: "",
       new_password_confirmation: "",
-      error: null,
+      ...this.cleanErrors,
     };
+
     this.initialState = this.state;
 
     auth();
 
     this.resetPassword = this.resetPassword.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.checkErrors = this.checkErrors.bind(this);
+  }
+
+  checkErrors() {
+    const {
+      current_password,
+      new_password,
+      new_password_confirmation,
+    } = this.state;
+
+    this.setState(this.cleanErrors);
+
+    if (!current_password) {
+      this.setState({ error: ERROR_PASSWORD_BLANK, error_current: true });
+      return false;
+    }
+    if (!new_password) {
+      this.setState({ error: ERROR_PASSWORD_BLANK, error_new: true });
+      return false;
+    }
+    if (!new_password_confirmation) {
+      this.setState({
+        error: ERROR_PASSWORD_CONFIRMATION_BLANK,
+        error_new_confirm: true,
+      });
+      return false;
+    }
+    if (new_password !== new_password_confirmation) {
+      this.setState({
+        error: ERROR_PASSWORD_MATCH,
+        error_new: true,
+        error_new_confirm: true,
+      });
+      return false;
+    }
+    if (new_password.length < settings.MIN_PASSWORD_LENGTH) {
+      this.setState({
+        error: ERROR_SHORT_PASSWORD,
+        error_new: true,
+        error_new_confirm: true,
+      });
+      return false;
+    }
+    return true;
   }
 
   resetPassword(mutate) {
@@ -58,20 +110,7 @@ class ResetPasswordPage extends Component {
       new_password_confirmation,
     } = this.state;
 
-    if (!new_password || !current_password) {
-      this.setState({ error: ERROR_PASSWORD_BLANK });
-      return;
-    }
-    if (!new_password_confirmation) {
-      this.setState({ error: ERROR_PASSWORD_CONFIRMATION_BLANK });
-      return;
-    }
-    if (new_password !== new_password_confirmation) {
-      this.setState({ error: ERROR_PASSWORD_MATCH });
-      return;
-    }
-    if (new_password.length < settings.MIN_PASSWORD_LENGTH) {
-      this.setState({ error: ERROR_SHORT_PASSWORD });
+    if (!this.checkErrors()) {
       return;
     }
 
@@ -91,11 +130,11 @@ class ResetPasswordPage extends Component {
       this.state.new_password === this.state.new_password_confirmation &&
       this.state.error
     ) {
-      this.setState({ error: false });
+      this.checkErrors();
     }
   }
 
-  render() {
+  render(props, state) {
     return (
       <div id="root">
         <Toolbar text="Reset password" />
@@ -126,6 +165,7 @@ class ResetPasswordPage extends Component {
                       iconPosition="left"
                       type="password"
                       placeholder="Current password"
+                      error={state.error_current}
                       value={this.state.current_password}
                       onChange={this.handleChange}
                     />
@@ -137,6 +177,7 @@ class ResetPasswordPage extends Component {
                       iconPosition="left"
                       type="password"
                       placeholder="New password"
+                      error={state.error_new}
                       value={this.state.new_password}
                       onChange={this.handleChange}
                     />
@@ -148,6 +189,7 @@ class ResetPasswordPage extends Component {
                       iconPosition="left"
                       type="password"
                       placeholder="Confirm new password"
+                      error={state.error_new_confirm}
                       value={this.state.new_password_confirmation}
                       onChange={this.handleChange}
                     />
