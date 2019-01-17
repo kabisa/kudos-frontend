9f29d6f2a86dc301303debeb58aff3cdc8b8c3c6
Stefan Pahlplatz <stefan314@live.nl>
2019-01-17 13:54:22 +0100

    Member view

--- src/modules/manage-team/sections/Members.js
+++ src/modules/manage-team/sections/Members.js
@@ -12,6 +12,7 @@ export const GET_USERS = gql`
         id
         role
         user {
+          id
           name
           email
         }
@@ -28,7 +29,29 @@ export const DEACTIVATE_USER = gql`
   }
 `;
 
+export const ALTER_ROLE = gql`
+  mutation UpdateTeamMemberRole(
+    $role: TeamMemberRole!
+    $userId: ID!
+    $teamId: ID!
+  ) {
+    updateTeamMemberRole(role: $role, userId: $userId, teamId: $teamId) {
+      teamMember {
+        role
+        id
+      }
+      errors
+    }
+  }
+`;
+
 export class MemberSection extends Component {
+  constructor(props) {
+    super(props);
+
+    this.userId = localStorage.getItem(settings.USER_ID_TOKEN);
+  }
+
   render() {
     return (
       <div>
@@ -44,7 +67,7 @@ export class MemberSection extends Component {
           query={GET_USERS}
           variables={{ id: localStorage.getItem(settings.TEAM_ID_TOKEN) }}
         >
-          {({ loading, error, data }) => {
+          {({ loading, error, data, refetch }) => {
             if (loading) return "Loading...";
             if (error) return `Error! ${error.message}`;
 
@@ -68,41 +91,101 @@ export class MemberSection extends Component {
                           <Table.Cell>{item.user.email}</Table.Cell>
                           <Table.Cell>{item.role}</Table.Cell>
                           <Table.Cell>
-                            <Mutation
-                              mutation={DEACTIVATE_USER}
-                              onCompleted={() => {
-                                toast.info("User deactivated successfully!");
-                              }}
-                              refetchQueries={[
-                                {
-                                  query: GET_USERS,
-                                  variables: {
-                                    id: localStorage.getItem(
-                                      settings.TEAM_ID_TOKEN
-                                    ),
-                                  },
-                                },
-                              ]}
-                            >
-                              {(mutate, { loading }) => (
-                                <Button
-                                  color="red"
-                                  size="tiny"
-                                  loading={loading}
-                                  onClick={() => {
-                                    if (
-                                      confirm(
-                                        "Are you sure you want to deactivate this user?"
-                                      )
-                                    ) {
-                                      mutate({ variables: { id: item.id } });
-                                    }
+                            {this.userId !== item.user.id && (
+                              <div>
+                                <Mutation
+                                  mutation={ALTER_ROLE}
+                                  onCompleted={() => {
+                                    toast.info("Role updated successfully!");
+                                    refetch();
                                   }}
                                 >
-                                  Remove
-                                </Button>
-                              )}
-                            </Mutation>
+                                  {(mutate, { loading }) => (
+                                    <Button
+                                      color="green"
+                                      size="tiny"
+                                      icon="arrow up"
+                                      loading={loading}
+                                      disabled={item.role === "admin"}
+                                      onClick={() =>
+                                        mutate({
+                                          variables: {
+                                            role:
+                                              item.role === "member"
+                                                ? "moderator"
+                                                : "admin",
+                                            userId: item.user.id,
+                                            teamId: localStorage.getItem(
+                                              settings.TEAM_ID_TOKEN
+                                            ),
+                                          },
+                                        })
+                                      }
+                                    />
+                                  )}
+                                </Mutation>
+                                <Mutation
+                                  mutation={ALTER_ROLE}
+                                  onCompleted={() => {
+                                    toast.info("Role updated successfully!");
+                                    refetch();
+                                  }}
+                                >
+                                  {(mutate, { loading }) => (
+                                    <Button
+                                      color="yellow"
+                                      size="tiny"
+                                      icon="arrow down"
+                                      disabled={item.role === "member"}
+                                      loading={loading}
+                                      onClick={() =>
+                                        mutate({
+                                          variables: {
+                                            role:
+                                              item.role === "moderator"
+                                                ? "member"
+                                                : "moderator",
+                                            userId: item.user.id,
+                                            teamId: localStorage.getItem(
+                                              settings.TEAM_ID_TOKEN
+                                            ),
+                                          },
+                                        })
+                                      }
+                                    />
+                                  )}
+                                </Mutation>
+                                <Mutation
+                                  mutation={DEACTIVATE_USER}
+                                  onCompleted={() => {
+                                    toast.info(
+                                      "User deactivated successfully!"
+                                    );
+                                  }}
+                                >
+                                  {(mutate, { loading }) => (
+                                    <Button
+                                      color="red"
+                                      size="tiny"
+                                      icon="trash"
+                                      loading={loading}
+                                      onClick={() => {
+                                        if (
+                                          confirm(
+                                            "Are you sure you want to deactivate this user?"
+                                          )
+                                        ) {
+                                          mutate({
+                                            variables: { id: item.id },
+                                          });
+                                          refetch();
+                                        }
+                                      }}
+                                    />
+                                  )}
+                                </Mutation>
+                              </div>
+                            )}
                           </Table.Cell>
                         </Table.Row>
                       );
@@ -117,4 +200,5 @@ export class MemberSection extends Component {
     );
   }
 }
+
 export default MemberSection;
