6af152634be45f4a93f9d37327147748b9c3670a
max <max@jemoerder.nl>
2020-03-09 16:01:50 +0100

    Add CircleCI config.yml

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from '../src/App';
+import App from '../App';
+import { LoginPage } from "../modules/login";
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
