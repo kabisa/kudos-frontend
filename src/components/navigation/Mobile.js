1b672b4dff8bf2c6003d90c032be4a4672312032
max <max@jemoerder.nl>
2020-03-09 15:02:09 +0100

    Highlight active tab on mobile tab bar

--- src/components/navigation/Mobile.js
+++ src/components/navigation/Mobile.js
@@ -1,41 +1,38 @@
 import React from "react";
 import { Icon } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 import * as routes from "../../routes";
 import settings from "../../config/settings";
 import s from "./Mobile.module.scss";
 
-const IconComponent = ({ path, icon }) => {
-  const current = window.location.hash.substring(1).split("?")[0];
-  if (current !== path) {
-    const link = `${path}?transition=none`;
-    return (
-      <Link to={link} className={s.bottom_item}>
-        <Icon name={icon} size="large" className={s.bottom_icon} />
-      </Link>
-    );
-  }
+const IconComponent = ({ path, icon, history }) => {
+  const link = `${path}`;
   return (
-    <div className={s.bottom_item}>
-      <Icon color="blue" name={icon} size="large" className={s.bottom_icon} />
+    <Link to={link} className={s.bottom_item}>
+      <Icon name={icon} size="large" className={s.bottom_icon}
+            color={history.location.pathname === path ? "blue" : "black"}/>
+    </Link>
+  );
+};
+
+const mobile = (history) => {
+  const isLoggedIn = localStorage.getItem(settings.TEAM_ID_TOKEN);
+  return (
+    <div className={s.bottom_navigation}>
+      <IconComponent path={routes.PATH_SETTINGS} icon="settings" history={history}/>
+      {isLoggedIn && (
+        <IconComponent path={routes.PATH_STATISTICS} icon="chart bar" history={history}/>
+      )}
+      {isLoggedIn && (
+        <IconComponent path={routes.PATH_FEED} icon="home" history={history}/>
+      )}
+      {isLoggedIn && (
+        <IconComponent path={routes.PATH_NOTIFICATIONS} icon="bell" history={history}/>
+      )}
+      <IconComponent path={routes.PATH_USER} icon="user" history={history}/>
     </div>
   );
 };
 
-export default () => {
-  const isLoggedIn = localStorage.getItem(settings.TEAM_ID_TOKEN);
-  return (
-    <div className={s.bottom_navigation}>
-      <IconComponent path={routes.PATH_SETTINGS} icon="settings" />
-      {isLoggedIn && (
-        <IconComponent path={routes.PATH_STATISTICS} icon="chart bar" />
-      )}
-      {isLoggedIn && <IconComponent path={routes.PATH_FEED} icon="home" />}
-      {isLoggedIn && (
-        <IconComponent path={routes.PATH_NOTIFICATIONS} icon="bell" />
-      )}
-      <IconComponent path={routes.PATH_USER} icon="user" />
-    </div>
-  );
-};
+export default withRouter(mobile);
