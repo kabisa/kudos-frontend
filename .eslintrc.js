77ba8aa40a861ee59d2d0258caaef23342dc16a6
max <max@jemoerder.nl>
2020-03-12 11:15:05 +0100

    Add Prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'airbnb-typescript',
+        'prettier/@typescript-eslint',
+    ],
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+};
