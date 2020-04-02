0a0172927d72d041ec8a48f65fbcf9fb7aca67e6
max <max@jemoerder.nl>
2020-04-02 08:42:45 +0200

    Update environment vars

--- src/config/settings.default.js
+++ src/config/settings.default.js
@@ -7,9 +7,10 @@ export default {
   MIN_POST_MESSAGE_LENGTH: 4,
   MAX_POST_MESSAGE_LENGTH: 140,
 
-  // Network settings.
-  API_BASE_URL: process.env.API_BASE_URL
-    ? process.env.API_BASE_URL
+  // Create react app ignores any env vars that are not prefixed with REACT_APP_
+  // https://create-react-app.dev/docs/adding-custom-environment-variables/
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL
+    ? process.env.REACT_APP_API_BASE_URL
     : "http://localhost:3000",
 
   // localStorage settings.
