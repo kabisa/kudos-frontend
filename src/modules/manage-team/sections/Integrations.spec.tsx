d4c36f0acc5723e69b389887b101a59e86728c0d
max <max@jemoerder.nl>
2020-05-26 10:43:56 +0200

    Add remove slack button

--- src/modules/manage-team/sections/Integrations.spec.tsx
+++ src/modules/manage-team/sections/Integrations.spec.tsx
@@ -5,8 +5,9 @@ import { act } from 'react-dom/test-utils';
 import {
   findByTestId, mockLocalstorage, wait, withMockedProviders,
 } from '../../../spec_helper';
-import IntegrationsSection, { GET_TEAM_INTEGRATIONS } from './Integrations';
+import IntegrationsSection, { GET_TEAM_INTEGRATIONS, REMOVE_SLACK } from './Integrations';
 
+let mutationCalled = false;
 const mocksWithoutSlack = [
   {
     request: {
@@ -37,6 +38,37 @@ const mocksWitSlack = [
       },
     },
   },
+  {
+    request: {
+      query: REMOVE_SLACK,
+      variables: { teamId: '1' },
+    },
+    result: () => {
+      mutationCalled = true;
+      return {
+        data: {
+          removeSlack: {
+            team: {
+              id: '1',
+            },
+          },
+        },
+      };
+    },
+  },
+  {
+    request: {
+      query: GET_TEAM_INTEGRATIONS,
+      variables: { id: '1' },
+    },
+    result: {
+      data: {
+        teamById: {
+          slackTeamId: 'someId',
+        },
+      },
+    },
+  },
 ];
 
 const mocksWithError = [
@@ -53,6 +85,7 @@ let wrapper: ReactWrapper;
 let history: History;
 const setup = (mocks: any) => {
   history = createMemoryHistory();
+  mutationCalled = false;
   wrapper = mount(withMockedProviders(<IntegrationsSection history={history} />, mocks));
 };
 
@@ -77,22 +110,75 @@ describe('<IntegrationsSection />', () => {
     });
   });
 
-  it('shows the slack button if the team has no slack id', async () => {
-    await act(async () => {
-      await wait(0);
-      await wrapper.update();
+  describe('not connected to Slack', () => {
+    beforeEach(() => {
+      setup(mocksWithoutSlack);
+    });
 
-      expect(findByTestId(wrapper, 'slack-button').length).toBe(1);
+    it('shows the slack disconnected container', async () => {
+      await act(async () => {
+        await wait(0);
+        await wrapper.update();
+
+        expect(findByTestId(wrapper, 'slack-disconnected-container').length).toBe(1);
+      });
+    });
+
+    it('doesn\'t show the slack connected container', async () => {
+      await act(async () => {
+        await wait(0);
+        await wrapper.update();
+
+        expect(findByTestId(wrapper, 'slack-connected-container').length).toBe(0);
+      });
+    });
+
+    it('redirects to the correct url', async () => {
+      await act(async () => {
+        await wait(0);
+        await wrapper.update();
+
+        const btn = findByTestId(wrapper, 'connect-slack-button');
+
+        expect(btn.prop('href')).toEqual('http://localhost:3000/auth/slack/team/1');
+      });
     });
   });
 
-  it('doesnt show the slack button if the team has a slack id', async () => {
-    setup(mocksWitSlack);
-    await act(async () => {
-      await wait(0);
-      await wrapper.update();
+  describe('connected to slack', () => {
+    beforeEach(() => {
+      setup(mocksWitSlack);
+    });
 
-      expect(findByTestId(wrapper, 'slack-button').length).toBe(0);
+    it('shows the connected to slack container', async () => {
+      await act(async () => {
+        await wait(0);
+        await wrapper.update();
+
+        expect(findByTestId(wrapper, 'slack-connected-container').length).toBe(1);
+      });
+    });
+
+    it('doesnt show the not connected to slack container', async () => {
+      await act(async () => {
+        await wait(0);
+        await wrapper.update();
+
+        expect(findByTestId(wrapper, 'slack-disconnected-container').length).toBe(0);
+      });
+    });
+
+    it('calls the disconnect mutation', async () => {
+      await act(async () => {
+        await wait(0);
+        await wrapper.update();
+
+        findByTestId(wrapper, 'remove-slack-btn').hostNodes().simulate('click');
+
+        await wait(0);
+
+        expect(mutationCalled).toBe(true);
+      });
     });
   });
 });
