293eee77356191ef8144836489cb9a8d634a7b75
Roy <roy.appeldoorn@gmail.com>
2023-08-10 10:52:50 +0200

    Add prettier as linter and show as warnings
    Also updated the graphql package to the newest version.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,27 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
-    parserOptions: {
-        project: './tsconfig.json',
-    },
-    extends: [
-        'airbnb-typescript',
-    ],
-    rules: {
-        "react/jsx-props-no-spreading": 0,
-        "import/no-cycle": 0,
-        "react/destructuring-assignment": 0,
-        "import/prefer-default-export": 0,
-        "react/jsx-one-expression-per-line": 0,
-        "max-len": ["error", {"code": 120}],
-        "react/button-has-type": 0,
-        "quotes": "double",
-    }
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  rules: {
+    "react/jsx-props-no-spreading": 0,
+    "import/no-cycle": 0,
+    "react/destructuring-assignment": 0,
+    "import/prefer-default-export": 0,
+    "max-len": ["error", { code: 120 }],
+    "react/button-has-type": 0,
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-unused-vars": 1,
+  },
 };
