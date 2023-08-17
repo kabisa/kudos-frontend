f77defa3399016db424b2450364c5154f5efe87d
Roy <roy.appeldoorn@gmail.com>
2023-08-17 15:41:59 +0200

    Delete unused component

--- src/modules/feed/components/index.ts
+++ src/modules/feed/components/index.ts
@@ -1,5 +1,4 @@
 export * from './Transaction';
-export { default as ActionButton } from './ActionButton/ActionButton';
 export { default as UserDropdown } from './UserDropdown/UserDropdown';
 export { default as GuidelineInput } from './GuidelineInput/GuidelineInput';
 export { default as RightRail } from './RightRail';
