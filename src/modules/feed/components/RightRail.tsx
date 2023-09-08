92a483d0121c06faa5244eddc61af52b59f966da
Sander Camp <sander.camp@kabisa.nl>
2023-09-08 14:09:55 +0200

    Improve statistics styling (somewhat)

--- src/modules/feed/components/RightRail.tsx
+++ src/modules/feed/components/RightRail.tsx
@@ -7,6 +7,7 @@ import { Statistics } from "../../statistics";
 
 import s from "./Rail.module.scss";
 import Segment from "../../../components/atoms/Segment";
+import moment from 'moment/moment';
 
 type RightRailProps = {
     className?: string;
@@ -14,6 +15,10 @@ type RightRailProps = {
 
 const RightRail: React.FC<RightRailProps> = ({ className }) => (
     <Segment className={ classNames(s.rail, className) } data-testid="right-tail">
+        <div className={ s.header }>
+            <h1 className={s.kudo_header}>₭udometer</h1>
+            <p className={s.today}>{moment().format("MMMM Do, YYYY")}</p>
+        </div>
         <Statistics />
         <KabisaLogo className={ s.logo }/>
     </Segment>
