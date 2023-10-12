7db0688a1b85fdc27820967092a69529749ab4d8
Marciano Schildmeijer <marciano@kabisa.nl>
2023-10-12 15:05:02 +0200

    Add button types.

--- src/ui/Button/index.tsx
+++ src/ui/Button/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Button as UIButton } from "@sandercamp/ui-components";
 
+export const ButtonVariants = ["primary", "secondary"] as const;
 export type ButtonProps = {
-  variant: "primary";
+  variant: (typeof ButtonVariants)[number];
   text: string;
 };
 const Button = ({ variant = "primary", text }: ButtonProps) => (
