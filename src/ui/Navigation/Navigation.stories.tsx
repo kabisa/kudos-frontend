2acfc895dcf520523e813e2b5946ab7935da81b7
Marciano Schildmeijer <marciano@kabisa.nl>
2023-10-20 12:45:40 +0200

    Refactor code climate issue

--- src/ui/Navigation/Navigation.stories.tsx
+++ src/ui/Navigation/Navigation.stories.tsx
@@ -5,46 +5,48 @@ import { Icon, Link } from "@sandercamp/ui-components";
 
 export const NavigationStory = (props: NavigationProps) => (
   <Navigation {...props}>
-    {(isDesktop) =>
-      isDesktop ? (
-        <>
-          <li>
-            <Link theme="light" href="https://google.com">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link theme="light" href="https://google.com">
-              Home
-            </Link>
-          </li>
-        </>
-      ) : (
-        <>
-          <li>
-            <Link theme="light" href="https://google.com">
-              <Icon name="home" />
-            </Link>
-          </li>
-          <li>
-            <Link theme="light" href="https://google.com">
-              <Icon name="home" />
-            </Link>
-          </li>
-          <li>
-            <Link theme="light" href="https://google.com">
-              <Icon name="home" />
-            </Link>
-          </li>
-          <li>
-            <Link theme="light" href="https://google.com">
-              <Icon name="home" />
-            </Link>
-          </li>
-        </>
-      )
-    }
+    {(isDesktop) => (isDesktop ? <DesktopNav /> : <MobileNav />)}
   </Navigation>
+);
+
+const DesktopNav = () => (
+  <>
+    <li>
+      <Link theme="light" href="https://google.com">
+        Home
+      </Link>
+    </li>
+    <li>
+      <Link theme="light" href="https://google.com">
+        Home
+      </Link>
+    </li>
+  </>
+);
+
+const MobileNav = () => (
+  <>
+    <li>
+      <Link theme="light" href="https://google.com">
+        <Icon name="home" />
+      </Link>
+    </li>
+    <li>
+      <Link theme="light" href="https://google.com">
+        <Icon name="home" />
+      </Link>
+    </li>
+    <li>
+      <Link theme="light" href="https://google.com">
+        <Icon name="home" />
+      </Link>
+    </li>
+    <li>
+      <Link theme="light" href="https://google.com">
+        <Icon name="home" />
+      </Link>
+    </li>
+  </>
 );
 
 const meta: Meta<typeof Navigation> = {
