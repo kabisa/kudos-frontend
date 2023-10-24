c2d20b1bf27da3d4f75c20067d161916e12957be
Marciano Schildmeijer <marciano@kabisa.nl>
2023-10-24 16:28:19 +0200

    Icon buttons.

--- src/ui/Button/index.tsx
+++ src/ui/Button/index.tsx
@@ -1,17 +1,35 @@
 import React from "react";
-import { Button as UIButton } from "@sandercamp/ui-components";
+import { Icon, Button as UIButton } from "@sandercamp/ui-components";
+import styles from "./styles.module.css";
 
 export const ButtonVariants = ["primary", "secondary"] as const;
 export const ButtonStates = ["default", "disabled"] as const;
 
+type IconProps = {
+  icon: string;
+  text?: string;
+};
+
+type TextProps = {
+  text: string;
+  icon?: string;
+};
+
 export type ButtonProps = {
   variant?: (typeof ButtonVariants)[number];
   state?: (typeof ButtonStates)[number];
-  children: React.ReactNode;
-};
-const Button = ({ variant = "primary", state, children }: ButtonProps) => (
-  <UIButton variant={variant} disabled={state === "disabled"}>
-    {children}
+} & (IconProps | TextProps);
+
+const Button = ({ variant = "primary", state, text, icon }: ButtonProps) => (
+  <UIButton
+    variant={variant}
+    disabled={state === "disabled" || false}
+    className={styles.button}
+  >
+    <span>
+      {icon && <Icon className={styles.icon} name={icon} />}
+      {text && <span>{text}</span>}
+    </span>
   </UIButton>
 );
 
