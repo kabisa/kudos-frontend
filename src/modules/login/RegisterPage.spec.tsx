ce3e424b2a4476817478e11d70e61b31ecc8ed5a
Matthijs Groen <matthijs.groen@gmail.com>
2024-05-17 14:27:44 +0200

    Migrate RegisterPage to RTL

--- src/modules/login/RegisterPage.spec.tsx
+++ src/modules/login/RegisterPage.spec.tsx
@@ -1,14 +1,8 @@
-import { mount, ReactWrapper } from "enzyme";
-import { act } from "react-dom/test-utils";
 import { GraphQLError } from "graphql";
-import {
-  findByTestId,
-  simulateInputChange,
-  wait,
-  withMockedProviders,
-} from "../../spec_helper";
+import { withMockedProviders } from "../../spec_helper";
 import { RegisterPage } from "./index";
 import { MUTATION_REGISTER } from "./RegisterPage";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 
 let mutationCalled = false;
 const mocks = [
@@ -37,14 +31,12 @@ const mocks = [
       };
     },
   },
-];
-const mocksWithError = [
   {
     request: {
       query: MUTATION_REGISTER,
       variables: {
-        name: "Max",
-        email: "max@example.com",
+        name: "Min",
+        email: "min@example.com",
         password: "password",
       },
     },
@@ -54,161 +46,117 @@ const mocksWithError = [
   },
 ];
 
-describe.skip("<RegisterPage />", () => {
-  let wrapper: ReactWrapper;
-
+describe("<RegisterPage />", () => {
   beforeEach(() => {
     mutationCalled = false;
-    wrapper = mount(withMockedProviders(<RegisterPage />, mocks));
+    render(withMockedProviders(<RegisterPage />, mocks));
   });
 
   it("handles input correctly", async () => {
-    const component: any = wrapper.find("RegisterPage").instance();
+    const nameField = screen.getByLabelText("Name");
+    fireEvent.change(nameField, { target: { value: "Max" } });
 
-    await act(async () => {
-      expect(component.state.name).toBe("");
-      expect(component.state.email).toBe("");
-      expect(component.state.password).toBe("");
+    const emailField = screen.getByLabelText("Email");
+    fireEvent.change(emailField, { target: { value: "max@example.com" } });
 
-      simulateInputChange(wrapper, "name-input", "name", "Max");
-      simulateInputChange(wrapper, "email-input", "email", "max@example.com");
-      simulateInputChange(wrapper, "password-input", "password", "password");
+    const passwordField = screen.getByLabelText("Password");
+    fireEvent.change(passwordField, { target: { value: "password" } });
 
-      await wrapper.update();
-      expect(component.state.name).toBe("Max");
-      expect(component.state.email).toBe("max@example.com");
-      expect(component.state.password).toBe("password");
-    });
+    const submitButton = screen.getByRole("button", { name: "Register" });
+    submitButton.click();
+
+    await waitFor(() => expect(mutationCalled).toBe(true));
   });
 
   it("shows a message if the name is empty", async () => {
-    const component: any = wrapper.find("RegisterPage").instance();
+    const emailField = screen.getByLabelText("Email");
+    fireEvent.change(emailField, { target: { value: "max@example.com" } });
 
-    await act(async () => {
-      component.setState({ name: "", email: "email", password: "password" });
-      await wrapper.update();
+    const passwordField = screen.getByLabelText("Password");
+    fireEvent.change(passwordField, { target: { value: "password" } });
 
-      findByTestId(wrapper, "submit-button").hostNodes().simulate("submit");
+    const submitButton = screen.getByRole("button", { name: "Register" });
+    submitButton.click();
 
-      expect(component.state.error).toBe("You need to fill all fields.");
-      expect(
-        wrapper.containsMatchingElement(<p>You need to fill all fields.</p>),
-      ).toBe(true);
-    });
+    const header = screen.getByRole("heading", { name: "Unable to register" });
+    expect(header).toBeInTheDocument();
+    const message = screen.getByText("You need to fill all fields.");
+    expect(message).toBeInTheDocument();
   });
 
   it("shows a message if the email is empty", async () => {
-    const component: any = wrapper.find("RegisterPage").instance();
+    const nameField = screen.getByLabelText("Name");
+    fireEvent.change(nameField, { target: { value: "Max" } });
 
-    await act(async () => {
-      component.setState({ name: "Max", email: "", password: "password" });
-      await wrapper.update();
+    const passwordField = screen.getByLabelText("Password");
+    fireEvent.change(passwordField, { target: { value: "password" } });
 
-      findByTestId(wrapper, "submit-button").hostNodes().simulate("submit");
+    const submitButton = screen.getByRole("button", { name: "Register" });
+    submitButton.click();
 
-      expect(component.state.error).toBe("You need to fill all fields.");
-      expect(
-        wrapper.containsMatchingElement(<p>You need to fill all fields.</p>),
-      ).toBe(true);
-    });
-  });
-
-  it("shows a message if the password is empty", async () => {
-    const component: any = wrapper.find("RegisterPage").instance();
-
-    await act(async () => {
-      component.setState({ name: "Max", email: "email", password: "" });
-      await wrapper.update();
-
-      findByTestId(wrapper, "submit-button").hostNodes().simulate("submit");
-
-      expect(component.state.error).toBe("You need to fill all fields.");
-      expect(
-        wrapper.containsMatchingElement(<p>You need to fill all fields.</p>),
-      ).toBe(true);
-    });
+    const header = screen.getByRole("heading", { name: "Unable to register" });
+    expect(header).toBeInTheDocument();
+    const message = screen.getByText("You need to fill all fields.");
+    expect(message).toBeInTheDocument();
   });
 
   it("shows a message if the email is invalid", async () => {
-    const component: any = wrapper.find("RegisterPage").instance();
+    const nameField = screen.getByLabelText("Name");
+    fireEvent.change(nameField, { target: { value: "Max" } });
 
-    await act(async () => {
-      component.setState({
-        name: "Max",
-        email: "fakeEmail",
-        password: "password",
-      });
-      await wrapper.update();
+    const emailField = screen.getByLabelText("Email");
+    fireEvent.change(emailField, { target: { value: "invalidEmail" } });
 
-      findByTestId(wrapper, "submit-button").hostNodes().simulate("submit");
+    const passwordField = screen.getByLabelText("Password");
+    fireEvent.change(passwordField, { target: { value: "password" } });
 
-      expect(component.state.error).toBe("Invalid email.");
-      expect(wrapper.containsMatchingElement(<p>Invalid email.</p>)).toBe(true);
-    });
+    const submitButton = screen.getByRole("button", { name: "Register" });
+    submitButton.click();
+
+    const header = screen.getByRole("heading", { name: "Unable to register" });
+    expect(header).toBeInTheDocument();
+    const message = screen.getByText("Invalid email.");
+    expect(message).toBeInTheDocument();
   });
 
   it("shows a message if the password is too short", async () => {
-    const component: any = wrapper.find("RegisterPage").instance();
+    const nameField = screen.getByLabelText("Name");
+    fireEvent.change(nameField, { target: { value: "Max" } });
 
-    await act(async () => {
-      component.setState({
-        name: "Max",
-        email: "max@example.com",
-        password: "short",
-      });
-      await wrapper.update();
+    const emailField = screen.getByLabelText("Email");
+    fireEvent.change(emailField, { target: { value: "max@example.com" } });
 
-      findByTestId(wrapper, "submit-button").hostNodes().simulate("submit");
+    const passwordField = screen.getByLabelText("Password");
+    fireEvent.change(passwordField, { target: { value: "pass" } });
 
-      expect(component.state.error).toBe(
-        "Password needs to have a minimum of 8 characters.",
-      );
-      expect(
-        wrapper.containsMatchingElement(
-          <p>Password needs to have a minimum of 8 characters.</p>,
-        ),
-      ).toBe(true);
-    });
+    const submitButton = screen.getByRole("button", { name: "Register" });
+    submitButton.click();
+
+    const header = screen.getByRole("heading", { name: "Unable to register" });
+    expect(header).toBeInTheDocument();
+    const message = screen.getByText(
+      "Password needs to have a minimum of 8 characters.",
+    );
+    expect(message).toBeInTheDocument();
   });
 
   it("shows when there is an error", async () => {
-    wrapper = mount(withMockedProviders(<RegisterPage />, mocksWithError));
-    const component = wrapper.find("RegisterPage").instance();
+    const nameField = screen.getByLabelText("Name");
+    fireEvent.change(nameField, { target: { value: "Min" } });
 
-    await act(async () => {
-      component.setState({
-        name: "Max",
-        email: "max@example.com",
-        password: "password",
-      });
-      await wrapper.update();
+    const emailField = screen.getByLabelText("Email");
+    fireEvent.change(emailField, { target: { value: "min@example.com" } });
 
-      findByTestId(wrapper, "submit-button").hostNodes().simulate("submit");
+    const passwordField = screen.getByLabelText("Password");
+    fireEvent.change(passwordField, { target: { value: "password" } });
 
-      await wait(0);
-      await wrapper.update();
-
-      expect(findByTestId(wrapper, "error-message").text()).toBe("It broke");
+    const submitButton = screen.getByRole("button", { name: "Register" });
+    submitButton.click();
+    const header = await screen.findByRole("heading", {
+      name: "Unable to register",
     });
-  });
-
-  it("calls the mutation", async () => {
-    const component = wrapper.find("RegisterPage").instance();
-
-    await act(async () => {
-      component.setState({
-        name: "Max",
-        email: "max@example.com",
-        password: "password",
-      });
-      await wrapper.update();
-
-      findByTestId(wrapper, "submit-button").hostNodes().simulate("submit");
-
-      await wait(0);
-      await wrapper.update();
-
-      expect(mutationCalled).toBe(true);
-    });
+    expect(header).toBeInTheDocument();
+    const message = screen.getByText("It broke");
+    expect(message).toBeInTheDocument();
   });
 });
