a435ebe8b9a21e124068169e6b1c2217dcc597e5
Matthijs Groen <matthijs.groen@gmail.com>
2024-05-23 12:13:35 +0200

    Migrate MemberRow to RTL

--- src/modules/manage-team/sections/member/MemberRow.spec.tsx
+++ src/modules/manage-team/sections/member/MemberRow.spec.tsx
@@ -1,13 +1,7 @@
-import { mount, ReactWrapper } from "enzyme";
-import { act } from "react-dom/test-utils";
-import {
-  findByTestId,
-  mockLocalstorage,
-  wait,
-  withMockedProviders,
-} from "../../../../spec_helper";
+import { mockLocalstorage, withMockedProviders } from "../../../../spec_helper";
 import { MemberRow } from "./MemberRow";
 import { DEACTIVATE_USER } from "./Members";
+import { RenderResult, render, screen, waitFor } from "@testing-library/react";
 
 const membership = {
   id: "1",
@@ -39,10 +33,14 @@ const mocks = [
   },
 ];
 
-let wrapper: ReactWrapper;
 const refetch = jest.fn();
+let result: RenderResult | null = null;
 const setup = () => {
-  wrapper = mount(
+  if (result !== null) {
+    result.unmount();
+  }
+
+  result = render(
     withMockedProviders(
       <table>
         <tbody>
@@ -54,29 +52,31 @@ const setup = () => {
   );
 };
 
-describe.skip("<MemberRow />", () => {
+describe("<MemberRow />", () => {
   mockLocalstorage("5");
 
   beforeEach(() => {
     mutationCalled = false;
+    global.confirm = jest.fn(() => true);
     setup();
   });
 
   it("renders the membership information", () => {
-    expect(wrapper.containsMatchingElement(<td>max</td>)).toBe(true);
-    expect(wrapper.containsMatchingElement(<td>max@example.com</td>)).toBe(
-      true,
-    );
-    expect(wrapper.containsMatchingElement(<td>member</td>)).toBe(true);
+    expect(screen.getByRole("cell", { name: "max" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("cell", { name: "max@example.com" }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole("cell", { name: "member" })).toBeInTheDocument();
   });
 
-  it("calls the mutation", async () => {
-    await act(async () => {
-      findByTestId(wrapper, "deactivate-button").hostNodes().simulate("click");
+  it("calls the mutation on deactivation", async () => {
+    const deactivateButton = screen.getByRole("button", { name: "delete" });
+    deactivateButton.click();
+    expect(global.confirm).toHaveBeenCalledWith(
+      "Are you sure you want to delete this member?",
+    );
 
-      await wait(0);
-      await wrapper.update();
-
+    await waitFor(() => {
       expect(mutationCalled).toBe(true);
       expect(refetch).toBeCalledTimes(1);
     });
@@ -86,13 +86,15 @@ describe.skip("<MemberRow />", () => {
     mockLocalstorage("5");
     setup();
 
-    expect(wrapper.find(".button").length).toBe(3);
+    const buttons = screen.queryAllByRole("button");
+    expect(buttons).toHaveLength(3);
   });
 
   it("does not render the buttons if the membership is the current user", () => {
     mockLocalstorage("1");
     setup();
 
-    expect(wrapper.find(".button").length).toBe(0);
+    const buttons = screen.queryAllByRole("button");
+    expect(buttons).toHaveLength(0);
   });
 });
