d126f2c1c5260c0315d4f1762ce02d85acb34b1b
Matthijs Groen <matthijs.groen@gmail.com>
2024-05-24 12:08:17 +0200

    Migrate UserPage spec to RTL

--- src/modules/user/UserPage.spec.tsx
+++ src/modules/user/UserPage.spec.tsx
@@ -1,14 +1,14 @@
-import { mount, ReactWrapper } from "enzyme";
 import { createMemoryHistory, MemoryHistory } from "history";
-import { act } from "react-dom/test-utils";
 import { DISCONNECT_SLACK, GET_USER, UserPage } from "./UserPage";
-import {
-  findByTestId,
-  mockLocalstorage,
-  wait,
-  withMockedProviders,
-} from "../../spec_helper";
+import { mockLocalstorage, withMockedProviders } from "../../spec_helper";
 import { PATH_RESET_PASSWORD } from "../../routes";
+import {
+  render,
+  RenderResult,
+  screen,
+  waitFor,
+  within,
+} from "@testing-library/react";
 
 let mutationCalled = false;
 export const mocks = ({ slackId = "" } = { slackId: "" }) => [
@@ -65,140 +65,137 @@ const mocksWithSlackId = [
   },
 ];
 
-let wrapper: ReactWrapper;
 let history: MemoryHistory;
-
+let renderResult: RenderResult | null = null;
 const setup = async (mock: any) => {
+  if (renderResult) {
+    await waitFor(() => {
+      expect(screen.queryByText("Loading...")).toBeNull();
+    });
+
+    renderResult.unmount();
+  }
+
   history = createMemoryHistory();
   mutationCalled = false;
   mockLocalstorage("1");
 
-  await act(async () => {
-    wrapper = mount(withMockedProviders(<UserPage history={history} />, mock));
-  });
+  renderResult = render(
+    withMockedProviders(<UserPage history={history} />, mock),
+  );
 };
 
-describe.skip("<UserPage/>", () => {
+describe("<UserPage/>", () => {
+  const original = window;
   beforeEach(async () => {
+    window = Object.create(window);
+    const url = "http://dummy.com";
+    Object.defineProperty(window, "location", {
+      value: {
+        href: url,
+      },
+      writable: true, // possibility to override
+    });
+
     await setup(mocks());
   });
 
-  it("shows the component is loading", () => {
-    expect(findByTestId(wrapper, "loading").length).toBe(1);
+  afterEach(() => {
+    window = original;
+  });
+
+  it("shows the component is loading", async () => {
+    const loading = screen.getByText("Loading...");
+    expect(loading).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.queryByText("Loading...")).toBeNull();
+    });
   });
 
   it("shows the users name", async () => {
-    await act(async () => {
-      await wait(0);
-
-      wrapper.update();
-
-      expect(wrapper.containsMatchingElement(<h2>Max</h2>)).toBe(true);
-    });
-  });
-
-  it("doesnt show an image when the query hasn't loaded", () => {
-    expect(wrapper.containsMatchingElement(<img src="fakeAvatarUrl" />)).toBe(
-      false,
-    );
+    const name = await screen.findByRole("heading", { level: 2, name: "Max" });
+    expect(name).toBeInTheDocument();
   });
 
   it("shows the users avatar", async () => {
-    await act(async () => {
-      await wait(0);
+    const image = (await screen.findAllByRole("img"))[0];
 
-      wrapper.update();
+    expect(image.getAttribute("src")).toEqual("fakeAvatarUrl");
+  });
 
-      expect(wrapper.containsMatchingElement(<img src="fakeAvatarUrl" />)).toBe(
-        true,
-      );
+  it("shows a link to gravatar", async () => {
+    const link = await screen.findByRole("link", { name: "gravatar.com" });
+    expect(link.getAttribute("href")).toEqual("https://nl.gravatar.com/");
+  });
+
+  it("shows a link to the reset password page", async () => {
+    const resetPasswordButton = await screen.findByRole("button", {
+      name: "Change password",
     });
+    expect(resetPasswordButton).toBeInTheDocument();
   });
 
-  it("shows a link to gravatar", () => {
-    expect(
-      wrapper.containsMatchingElement(
-        <a href="https://nl.grvaatar.com/">gravatar.com</a>,
-      ),
-    );
-  });
-
-  it("shows a link to the reset password page", () => {
-    expect(wrapper.containsMatchingElement(<button>Change password</button>));
-  });
-
-  it("shows a logout button", () => {
-    expect(wrapper.containsMatchingElement(<button>Log out</button>));
+  it("shows a logout button", async () => {
+    const resetPasswordButton = await screen.findByRole("button", {
+      name: "Log out",
+    });
+    expect(resetPasswordButton).toBeInTheDocument();
   });
 
   it("navigates to the reset password page", async () => {
-    await act(async () => {
-      const button = findByTestId(wrapper, "reset-password-btn").hostNodes();
+    const resetPasswordButton = await screen.findByRole("button", {
+      name: "Change password",
+    });
+    resetPasswordButton.click();
 
-      button.simulate("click", { button: 0 });
-
-      wrapper.update();
-
+    await waitFor(() => {
       expect(history.location.pathname).toBe(PATH_RESET_PASSWORD);
     });
   });
 
-  describe.skip("not connected to slack", () => {
+  describe("not connected to slack", () => {
     beforeEach(async () => {
       await setup(mocks());
     });
 
     it("shows the connect to slack part if the slack id is null", async () => {
-      await act(async () => {
-        await wait(0);
-        await wrapper.update();
-
-        expect(findByTestId(wrapper, "register-slack").hostNodes().length).toBe(
-          1,
-        );
-      });
+      const connectPart = await screen.findByTestId("register-slack");
+      expect(connectPart).toBeInTheDocument();
     });
 
     it("has the correct url for the connect button", async () => {
-      await act(async () => {
-        await wait(0);
-        await wrapper.update();
+      const connectPart = await screen.findByTestId("register-slack");
+      expect(connectPart).toBeInTheDocument();
 
-        const btn = findByTestId(wrapper, "connect-slack-btn").hostNodes();
-
-        expect(btn.prop("href")).toEqual(
-          "http://localhost:3000/auth/slack/user/1",
-        );
+      const button = within(connectPart).getByRole("button", {
+        // The alt text of the image is also counting toward the button name
+        name: "Connect account Connect account",
       });
+      button.click();
+      expect(button).toBeInTheDocument();
+
+      expect(window.location.href).toEqual(
+        "http://localhost:3000/auth/slack/user/1",
+      );
     });
   });
 
-  describe.skip("connected to slack", () => {
+  describe("connected to slack", () => {
     beforeEach(async () => {
       await setup(mocksWithSlackId);
     });
 
     it("shows the user is connected to slack if the slack id is not null", async () => {
-      await act(async () => {
-        await wait(0);
-        await wrapper.update();
-
-        expect(
-          findByTestId(wrapper, "slack-connected").hostNodes().length,
-        ).toBe(1);
-      });
+      const connectedPart = await screen.findByTestId("slack-connected");
+      expect(connectedPart).toBeInTheDocument();
     });
 
     it("calls the disconnect mutation", async () => {
-      await act(async () => {
-        await wait(0);
-        await wrapper.update();
+      const connectedPart = await screen.findByTestId("slack-connected");
 
-        findByTestId(wrapper, "disconnect-slack-btn")
-          .hostNodes()
-          .simulate("click");
-
-        await wait(0);
+      within(connectedPart).getByRole("button").click();
+      await waitFor(() => {
         expect(mutationCalled).toBe(true);
       });
     });
